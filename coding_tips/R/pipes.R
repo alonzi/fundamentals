8acbeacbeb942237dcefeb4a8579381ac2ef9744
pete <pete.alonzi@gmail.com>
2017-06-15 18:14:20 -0400

    add R pipe example

--- coding_tips/R/pipes.R
+++ coding_tips/R/pipes.R
@@ -6,8 +6,23 @@ f(g(x,y),z) # is a little harder to read
 # R let's you pipe objects into arguments of functions with %>%
 
 f(g(x,y),z)
+
 # becomes
+
 x %>%
   g(y) %>%
   f(z)
+
+
+# here's another example from http://kbroman.org/hipsteR/
+
+round(exp(diff(log(x))), 1)
+
+#becomes
+
+x %>% 
+  log() %>%
+  diff() %>%
+  exp() %>%
+  round(1)
   
